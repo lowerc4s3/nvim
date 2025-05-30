52ac879dedda8111d78f82abb4440a0de5e5e97b
lowerc4s3 <kkirill03@gmail.com>
2025-05-30 17:52:55 +0300

    fix(ts): use get_installed after treesitter update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
         build = ':TSUpdate',
         dependencies = 'OXY2DEV/markview.nvim',
         config = function()
-            local parsers = require("nvim-treesitter.config").installed_parsers()
+            local parsers = require("nvim-treesitter").get_installed()
             if #parsers ~= 0 then
                 vim.api.nvim_create_autocmd('FileType', {
                     pattern = parsers,
