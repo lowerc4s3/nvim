cfa1bb1176d6acbaedfe1eb2f74d1469d2820cf5
lowerc4s3 <kkirill03@gmail.com>
2025-06-10 00:47:55 +0300

    feat(editor): add various textobjects

---
+++ lua/plugins/editor/various_textobjects.lua
@@ -0,0 +1,17 @@
+return {
+    'chrisgrieser/nvim-various-textobjs',
+    event = 'VeryLazy',
+    opts = {},
+    keys = {
+        { 'is', '<cmd>lua require("various-textobjs").subword("inner")<cr>', mode = { 'x', 'o' } },
+        { 'as', '<cmd>lua require("various-textobjs").subword("outer")<cr>', mode = { 'x', 'o' } },
+        { 'iS', 'is', mode = { 'x', 'o' } },
+        { 'aS', 'as', mode = { 'x', 'o' } },
+        { 'as', '<cmd>lua require("various-textobjs").subword("outer")<cr>', mode = { 'x', 'o' } },
+        { 'ik', '<cmd>lua require("various-textobjs").key("inner")<cr>', mode = { 'x', 'o' } },
+        { 'ak', '<cmd>lua require("various-textobjs").key("outer")<cr>', mode = { 'x', 'o' } },
+        { 'iv', '<cmd>lua require("various-textobjs").value("inner")<cr>', mode = { 'x', 'o' } },
+        { 'av', '<cmd>lua require("various-textobjs").value("outer")<cr>', mode = { 'x', 'o' } },
+        { 'gG', '<cmd>lua require("various-textobjs").entireBuffer()<cr>', mode = { 'x', 'o' } },
+    },
+}
