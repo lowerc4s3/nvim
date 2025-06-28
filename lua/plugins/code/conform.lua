4c44b72f2e63409360520c8620b1bfa86e0df2af
lowerc4s3 <kkirill03@gmail.com>
2025-06-28 16:00:13 +0300

    feat(code/conform): enable typst formatting

--- lua/plugins/code/conform.lua
+++ lua/plugins/code/conform.lua
@@ -5,6 +5,7 @@ return {
         formatters_by_ft = {
             lua = { 'stylua' },
             python = { 'isort', 'black' },
+            typst = { 'typstyle' },
         },
         default_format_opts = { lsp_format = 'fallback' },
         format_after_save = function(_)
