71b90b60b9ae4ffdaad6ad19052be72a9a89f97a
lowerc4s3 <kkirill03@gmail.com>
2025-06-28 15:43:46 +0300

    refactor(keymaps): remove buffer navigation keymaps (use ]b instead)

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,7 +2,7 @@ local map = vim.keymap.set
 
 map({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true, desc = 'Disable space mapping' })
 
-map('n', '<cr>', 'ciw', { silent = true, desc = 'Replace the word under cursor' })
+-- map('n', '<cr>', 'ciw', { silent = true, desc = 'Replace the word under cursor' })
 
 map('v', 'y', 'ygv<esc>', { silent = true, desc = 'Make cursor stay when yanking in visual mode' })
 
@@ -16,8 +16,6 @@ map('i', '<C-j>', '<Down>', { silent = true, desc = 'Move down' })
 map('i', '<C-k>', '<Up>', { silent = true, desc = 'Move up' })
 map('i', '<C-l>', '<Right>', { silent = true, desc = 'Move left' })
 
-map('n', 'L', '<cmd>bnext<cr>', { silent = true, desc = 'Go to next buffer' })
-map('n', 'H', '<cmd>bprev<cr>', { silent = true, desc = 'Go to previous buffer' })
 map('n', '<Tab>', '<cmd>Telescope buffers<cr>', { silent = true, desc = 'List open buffers' })
 
 map('n', '<ESC>', ':noh<CR>', { silent = true, desc = 'Disable search highlight' })
